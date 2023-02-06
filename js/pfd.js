38e8624b20fc75ce3e4e683bba219740c4ce43f1
nehCnitsuJ <s10222137@connect.np.edu.sg>
2023-02-06 22:45:47 +0800

    Updated pfd experience blog

--- js/pfd.js
+++ js/pfd.js
@@ -9,3 +9,18 @@ scrollTopBtn.addEventListener("click", () => {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 });
+
+//Our clients - Swiper
+var swiper = new Swiper(".client-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
